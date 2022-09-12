ae0a5311ed43daee0ffd49d62bfd25651770f4c8
Karye <karye@users.noreply.github.com>
2022-09-12 12:19:18 +0200

    Börjat med if-satser

---
+++ Kapitel-3/Labb1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Labb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Does i move? ");
+            string svar = Console.ReadLine();
+            if (svar == "no")
+            {
+                Console.WriteLine("Should it? ");
+                if (svar == "no")
+                {
+                    Console.WriteLine("No problem!");
+                }
+                else
+                {
+                    Console.WriteLine("Use WD-40");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Should it? ");
+                if (svar == "yes")
+                {
+                    Console.WriteLine("No problem!");
+                }
+                else
+                {
+                    Console.WriteLine("Use Duct tape");
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel-3/Villkor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Villkor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+/*             Console.Write("Vg ange ditt lösenord: ");
+            string svar = Console.ReadLine();
+            
+            // Kontrollera att lösenordet är rätt
+            if (svar == "abc123")
+            {
+                Console.WriteLine("Bra nu kan du jobba vidare");
+                Console.WriteLine("blbla");
+            }
+            else if (svar == "def456")
+            {
+                Console.WriteLine("Ja, rätt lösenord");
+            }
+            else
+            {
+                Console.WriteLine("Fel lösenord");
+            } */
+
+            Console.Write("Är lampan inkopplad? (ja/nej)");
+            string svar = Console.ReadLine();
+            if (svar == "nej")
+            {
+                Console.WriteLine("Koppla in lampan");
+            }
+            else
+            {
+                Console.Write("Är glödlampan utbränd? ");
+                svar = Console.ReadLine();
+                if (svar == "ja")
+                {
+                    Console.WriteLine("Ersäatt lampan");
+                }
+                else
+                {
+                    Console.WriteLine("Köp ny lampa");
+                }
+            }
+        }
+    }
+}
