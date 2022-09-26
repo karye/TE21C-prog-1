9fb719382d1f318cb2180c0726ee86d0497161ad
Karye <karye@users.noreply.github.com>
2022-09-26 11:10:54 +0200

    Tränat på loopar, slump, loopräknare

---
+++ Kapitel-4/Lyckohjul/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lyckohjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Lyckohjulet!");
+            Random generator = new Random();
+
+            // Loopen
+            int varv = 1;
+            while (true)
+            {
+                // När användaren kör programmet får hen välja ett tal från 1 till 10 som hen tror att lyckohjulet kommer att stanna på
+                Console.Write("Välj ett tal från 1 till 10: ");
+                string gissningString = Console.ReadLine();
+                int gissning = int.Parse(gissningString);
+
+                // Slumpa därefter fram ett tal från 1 till 10 och säg till användaren vilket tal det blev
+                int slumptal = generator.Next(1, 11);
+                Console.WriteLine($"Lyckohjulet hamnade på {slumptal}");
+
+                // Berätta också för användaren om hen vann eller förlorade
+                if (gissning == slumptal)
+                {
+                    Console.WriteLine("Du vann!");
+                }
+                else
+                {
+                    Console.WriteLine("Du förlorade!");
+                }
+
+                // Räkna upp varv
+                varv++;
+
+                // Avsluta när loopen gått 3 varv
+                if (varv == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel-4/Menyer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Menyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Ett menyprogram!");
+
+            // Slumpgeneratorn
+            Random generator = new Random();
+
+            // Programloopen
+            while (true)
+            {
+                // Visa en meny
+                Console.WriteLine("1. Slumpa fram ett tal 1-6");
+                Console.WriteLine("2. Slumpa fram ett tal 1-100");
+                Console.WriteLine("3. Avsluta");
+                Console.Write("Välj ett alternativ: ");
+                string val = Console.ReadLine();
+
+                // Utför det användaren valt
+                if (val == "1")
+                {
+                    int slumptal = generator.Next(1, 7);
+                    Console.WriteLine($"Slumptalet är {slumptal}");
+                }
+                else if (val == "2")
+                {
+                    int slumptal = generator.Next(1, 101);
+                    Console.WriteLine($"Slumptalet är {slumptal}");
+                }
+                else if (val == "3")
+                {
+                    // Avslutar loopen
+                    Console.WriteLine("Hejdå!");
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt alternativ, välj 1-3");
+                }
+            }
+
+        }
+    }
+}
