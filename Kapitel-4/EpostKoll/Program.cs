6b54f592992f1cd439c37fcd2645f5a524c777f5
karye <karye.webb@gmail.com>
2022-10-17 11:08:35 +0200

    Tränat på textmanipulering

---
+++ Kapitel-4/EpostKoll/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace EpostKoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Kolla om epost är i korrekt format!");
+
+            // Tex karim@bredband.net
+            // Namndelen = karim
+            // Domändelen = bredband
+            // Landdelen = se
+
+            // Mata in epostadress
+            Console.Write("Mata in epostadress: ");
+            string epost = Console.ReadLine();
+            
+            // Skriv ut längden
+            //Console.WriteLine($"Antal inmattade tecken är {epost.Length}");
+
+            // Rensa bort mellanslagen
+            epost = epost.Trim();
+
+            // Skriv ut längden igen
+            //Console.WriteLine($"Antal inmattade tecken är {epost.Length} efter trimning");
+
+            // Kolla att inmattad epost är i korrekt format, innehållet @-tecknet
+            if (epost.Contains("@") && epost.Contains("."))
+            {
+                Console.WriteLine("Ser ut som en epostadress");
+
+                // Plockat ut delarna i epostadressen
+                // Hitta @-tecknet
+                int index = epost.IndexOf("@");
+                Console.WriteLine("@ finns på position " + index);
+
+                // Plocka ut alla tecken före @-tecknet
+                Console.WriteLine(epost.Substring(0, index));
+
+                // Skriv ut allt efter @-tecknet
+                string delBakom = epost.Substring(index + 1);
+                Console.WriteLine(delBakom);
+
+                // Dela upp i domändel och landdel
+                // Steg 1: hitta "."
+                index = delBakom.IndexOf(".");
+
+                // Skriv ut det som är före "."
+                Console.WriteLine(delBakom.Substring(0, index));
+
+                // Skriv ut det som är bakom "."
+                Console.WriteLine(delBakom.Substring(index + 1));
+            }
+            else
+            {
+                Console.WriteLine("Vad är det här för något?");
+            }
+
+        }
+    }
+}
---
+++ Kapitel-4/Namn/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Namn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Byta ordningen på förnamn och efternamn");
+
+            // Läs in "förnamn efternamn" och skriv ut "efternamn förnamn"
+            Console.Write("Ange förnamn efternamn: ");
+            string namn = Console.ReadLine();
+            
+            // Använd .IndexOf() och .SubString()
+            // Leta efter " "
+            int index = namn.IndexOf(" ");
+
+            string förnamn = namn.Substring(0, index);
+            string efternamn = namn.Substring(index + 1);
+
+            // Skriv ut omvänt
+            Console.WriteLine(efternamn + " " + förnamn);
+        }
+    }
+}
