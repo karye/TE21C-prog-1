65a4422a3891fb6f7b4d6cfd9bb60296663c4aa1
karye <karye.webb@gmail.com>
2022-10-24 12:02:35 +0200

    Tränat på array och loopar

---
+++ Kapitel-5/GissaOrd/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GissaOrd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gissa ordet!");
+
+            // Skapa en array på 10 djur
+            string[] djuren = {"Sardin", "Hare", "Katt", "Mås", "Häst", "Val", "Orm", "Hund", "Örn", "Lax"};    // index = 0, 1, 2, .. 8, 9
+
+            // Slumpa ett tal 0-9
+            Random generator = new Random();
+            int slumptal = generator.Next(0, 10);
+            //Console.WriteLine(slumptal);
+
+            // Skriv ut djuret mha slumptal som index
+            //Console.WriteLine(djuren[slumptal]);
+
+            // Gissa 5 ggr max
+            for (int i = 0; i < 5; i++)
+            {
+                // Fråga användaren att gissa ett djur
+                Console.Write("Gissa ett djur: ");
+                string gissning = Console.ReadLine();
+                
+                // Kolla om gissning är rätt
+                if (gissning.ToLower() == djuren[slumptal].ToLower())
+                {
+                    Console.WriteLine("Bra gissat!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Fel! Försök igen!");
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel-5/GissaSpelet/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Skapa en array på 10 ord, valfritt tema
+Slumpa fram ett ord
+Fråga användaren hur många ggr hen vill gissa, fast max 10 och inte mindre än 1
+När användaren gissat rätt, berätta hur många ggr hen behövde för att gissa
+Utmaning: lägg till poäng, ju färre gånger desto högre poäng
+ */
+
+using System;
+
+namespace GissaSpelet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
