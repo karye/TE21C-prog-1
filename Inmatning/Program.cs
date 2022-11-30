9b2313b74075885eb39d0175acc35dd656e42310
karye <karye.webb@gmail.com>
2022-11-30 15:08:03 +0100

    Tränat på TryParse()

---
+++ Inmatning/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Inmatning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program för att räkna ut åldern!");
+
+            // Programloop
+            while (true)
+            {
+                // Räkna ut ålder
+                Console.Write("Vilket år är du född? ");
+                string år = Console.ReadLine();
+
+                // Vi ska använda int.TryParse()
+                bool kundeParsa = int.TryParse(år, out int årtal);
+                Console.WriteLine($"Resultat TryParse: {kundeParsa} och {årtal}");
+
+                if (kundeParsa == true)
+                {
+                    // Räkna ut ålder: året idag - året man är född
+                    int ålder = DateTime.Now.Year - årtal;
+                    Console.WriteLine($"Du är {ålder} år");
+                }
+                else
+                {
+                    Console.WriteLine("Fel inmatning! Vg mata in ett årtal!");
+                }
+
+                // Vill du köra igen?
+                Console.Write("Vill du köra igen? (j/n) ");
+                string svar = Console.ReadLine();
+                if (svar == "n")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel-5/Area/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 
+    Läs in längden (int)
+    Läs in bredden (int)
+    Skriv ut arean = längden * bredden
+ */
+using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Räkna ut arean!");
+
+            // Läs in längden (int)
+            Console.Write("Ange längden (heltal): ");
+            string längdenString = Console.ReadLine();
+            Console.Write("Ange bredden (heltal): ");
+            string breddenString = Console.ReadLine();
+
+            // Konvertera från string -> int mha int.Tryparse()
+            /*             bool kundeParsaLängden = int.TryParse(längdenString, out int längden);
+                        bool kundeParsaBredden = int.TryParse(breddenString, out int bredden);
+
+                        // Räkna om true
+                        if (kundeParsaLängden && kundeParsaBredden)
+                        {
+                            // Vi har två int
+                            Console.WriteLine($"arean = {längden * bredden}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel inmatning!");
+                        } */
+
+            while (!int.TryParse(längdenString, out int längden))
+            {
+                Console.Write("Ange längden (heltal): ");
+                string längdenString = Console.ReadLine();
+            }
+
+            while (!int.TryParse(breddenString, out int bredden))
+            {
+                Console.Write("Ange bredden (heltal): ");
+                string breddenString = Console.ReadLine();
+            }
+        }
+    }
+}
